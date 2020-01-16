c5ecfe4eec3ecebd786e936ea726413bd58c01db
SYN <39426808+0xC05@users.noreply.github.com>
2020-01-17 00:10:40 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -5,7 +5,10 @@ import numpy as np
 import pickle
 import RPi.GPIO as GPIO
 from time import sleep
-
+import os
+import re
+import serial
+import time 
 relay_pin = [26]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
@@ -18,41 +21,73 @@ with open('labels', 'rb') as f:
 camera = PiCamera()
 camera.resolution = (1280, 720)
 camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(1280, 720))
+ledPin = 11
+blinkDelay = .5
+ledOn = False
 
-faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-recognizer = cv2.face.createLBPHFaceRecognizer()
-recognizer.load("trainer.yml")
+userspath = os.getcwd() + '\\images\\'
+userspath = userspath.replace('\\','/')
+rfidlist = os.listdir(userspath)
+print rfidlist
 
-font = cv2.FONT_HERSHEY_SIMPLEX
 
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	frame = frame.array
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
-	for (x, y, w, h) in faces:
-		roiGray = gray[y:y+h, x:x+w]
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-		id_, conf = recognizer.predict(roiGray)
 
-		for name, value in dict.items():
-			if value == id_:
-				print(name)
+try:
+    while True:
+        read_serial = ser.readline()
+	read_serial = read_serial.rstrip()
+	count =0
+	print read_serial
 
-		if conf <= 70:
+	if read_serial in rfidlist:
+
+		rawCapture = PiRGBArray(camera, size=(1280, 720))
+
+		faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+		recognizer = cv2.face.createLBPHFaceRecognizer()
+		recognizer.load("trainer.yml")
+
+		font = cv2.FONT_HERSHEY_SIMPLEX
+		count =0
+		start =time.time()
+		for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+			frame = frame.array
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+			faces = faceCascade.detectMultiScale(gray, scaleFactor = 1.5, minNeighbors = 5)
+			for (x, y, w, h) in faces:
+				roiGray = gray[y:y+h, x:x+w]
+
+				id_, conf = recognizer.predict(roiGray)
+
+				for name, value in dict.items():
+					if value == id_:
+						print(name)
+				print(time.time())
+				if conf <= 60:
+					count = count +1
+				
+				if count > 5 or time.time() - start > 60:
+					break
+
+				else:
+					GPIO.output(relay_pin, 0)
+			print(start - time.time())
+			
+
+			if count > 5 or (time.time() - start) > 60:
+				break
+
+			rawCapture.truncate(0)
+
+		if count > 5:
+			print("ja beta ja jele zindagi")
 			GPIO.output(relay_pin, 1)
-			cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-			cv2.putText(frame, name + str(conf), (x, y), font, 2, (0, 0 ,255), 2,cv2.LINE_AA)
-
 		else:
-			GPIO.output(relay_pin, 0)
+			print("Bada a ya bakchod")
+		cv2.destroyAllWindows()
 
-	cv2.imshow('frame', frame)
-	key = cv2.waitKey(1)
-
-	rawCapture.truncate(0)
-
-	if key == 27:
-		break
-
-cv2.destroyAllWindows()
+except Exception, err:
+    GPIO.cleanup()
+    print Exception, err
